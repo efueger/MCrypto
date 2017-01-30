0a62232d9188d9c4fb4a0d74cdb72a1449574455
Aaron Sutton <ajsutton.dev@gmail.com>
2017-01-29 20:49:24 -0500

    Version Checker

--- src/updatechecker.rb
+++ src/updatechecker.rb
@@ -1,3 +1,5 @@
 # Check for updates
 
 puts "Checking for avalible updates..."
+webversion = %x{wget -q -O /tmp/mcrypto_v_$$ https://raw.githubusercontent.com/AJdev23/MCrypto/updater-testing/src/currentversion.conf; cat /tmp/mcrypto_v_$$; rm /tmp/mcrypto_v_$$ }
+puts webversion
