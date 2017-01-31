10670ba89d98aba573af06ffac0dc27845198493
Aaron Sutton <ajsutton.dev@gmail.com>
2017-01-30 22:25:55 -0500

    strange GPG

--- test/gentestkey.rb
+++ test/gentestkey.rb
@@ -1,2 +1,18 @@
 puts "Making a test key"
-%x{gpg --batch --gen-key ../test/testkey}
+%x{ cat >foo <<EOF
+         %echo Generating a basic OpenPGP key
+         Key-Type: DSA
+         Key-Length: 1024
+         Subkey-Type: ELG-E
+         Subkey-Length: 1024
+         Name-Real: Joe Tester
+         Name-Comment: with stupid passphrase
+         Name-Email: joe@foo.bar
+         Expire-Date: 0
+         Passphrase: abc
+         # Do a commit here, so that we can later print "done" :-)
+         %commit
+         %echo done
+    EOF
+  }
+%x{gpg --batch --generate-key foo}
