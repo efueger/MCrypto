6bad54c576fad7ac31d60dd05a1e63b8e19de3ec
Aaron Sutton <ajsutton.dev@gmail.com>
2017-01-30 22:45:24 -0500

    debug #2

--- src/getkeys.rb
+++ src/getkeys.rb
@@ -2,13 +2,15 @@ raw = %x{gpg --list-public-keys | grep uid}
 unparsed = Array.new
 parsed = Array.new()
 unparsed = raw.split("\n")
-puts "RAW: " + raw
-puts "SIZE " + unparsed.size
-# if unparsed.size > 0
-#   unparsed.each do |x|
-#     x.reverse!; pa = x.slice(0..(x.index("]") - 1)); pa.reverse!; sa = pa.strip; parsed.push(sa)
-#   end
-#   puts parsed
-# else
-#   puts "nokeys"
-# end
+if unparsed.size > 0
+  unparsed.each do |x|
+    if x.include? "]"
+      x.reverse!; pa = x.slice(0..(x.index("]") - 1)); pa.reverse!; sa = pa.strip; parsed.push(sa)
+    else
+      x.reverse!; pa = x.slice(0..(x.index("d") - 1)); pa.reverse!; sa = pa.strip; parsed.push(sa)
+    end
+  end
+  puts parsed
+else
+  puts "nokeys"
+end
