78a8401f0151f8a247ac5435330676f8d0164d07
Aaron Sutton <ajsutton.dev@gmail.com>
2017-01-31 04:09:41 -0500

    touch up ruby

--- src/getkeys.rb
+++ src/getkeys.rb
@@ -1,15 +1,16 @@
 raw = %x{gpg --list-public-keys | grep uid}
-unparsed = Array.new
 parsed = Array.new()
 unparsed = raw.split("\n")
 
 if unparsed.size > 0
   unparsed.each do |x|
     if x.include? "]"
-      x.reverse!; pa = x.slice(0..(x.index("]") - 1)); pa.reverse!; sa = pa.strip; parsed.push(sa)
+      varstring = "]"
     else
-      x.reverse!; pa = x.slice(0..(x.index("d") - 1)); pa.reverse!; sa = pa.strip; parsed.push(sa)
+      varstring = "d"
     end
+    x.reverse!; pa = x.slice(0..(x.index(varstring) - 1)); pa.reverse!; sa = pa.strip; parsed.push(sa)
+
   end
   puts parsed
 else
