bc01e4d04315757c24b7aa19255eba19d9de3f0f
Aaron Sutton <ajsutton.dev@gmail.com>
2017-01-30 22:03:49 -0500

    code restructure, prevent nil errors

--- src/getkeys.rb
+++ src/getkeys.rb
@@ -1,11 +1,12 @@
 raw = %x{gpg --list-public-keys | grep uid}
 unparsed = Array.new
 parsed = Array.new()
-unparsed = raw.split("\n")
-if unparsed.any?
+if !raw.nil?
+  unparsed = raw.split("\n")
   unparsed.each do |x|
     x.reverse!; pa = x.slice(0..(x.index("]") - 1)); pa.reverse!; sa = pa.strip; parsed.push(sa)
   end
-else puts "nokeys"
+  puts parsed
+else
+  puts "nokeys"
 end
-puts parsed
