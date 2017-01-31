0e76a2ffc696c810ea2c573efc996b4736b07828
Aaron Sutton <ajsutton.dev@gmail.com>
2017-01-30 21:38:21 -0500

    why is travis still broken...

--- src/getkeys.rb
+++ src/getkeys.rb
@@ -2,5 +2,7 @@ raw = %x{gpg --list-public-keys | grep uid}
 unparsed = Array.new
 parsed = Array.new()
 unparsed = raw.split("\n")
-unparsed.each { |x| x.reverse!; pa = x.slice(0..(x.index(']') - 1)); pa.reverse!; sa = pa.strip; parsed.push(sa)}
+unparsed.each do |x|
+  x.reverse!; pa = x.slice(0..(x.index("]") - 1)); pa.reverse!; sa = pa.strip; parsed.push(sa)
+end
 puts parsed
