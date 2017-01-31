8066126fa304c0753bf6f3538d35bcbd1478da90
Aaron Sutton <ajsutton.dev@gmail.com>
2017-01-30 22:10:40 -0500

    add .empty?

--- src/getkeys.rb
+++ src/getkeys.rb
@@ -1,7 +1,7 @@
 raw = %x{gpg --list-public-keys | grep uid}
 unparsed = Array.new
 parsed = Array.new()
-if !raw.nil?
+if !raw.nil? || raw.empty?
   unparsed = raw.split("\n")
   unparsed.each do |x|
     x.reverse!; pa = x.slice(0..(x.index("]") - 1)); pa.reverse!; sa = pa.strip; parsed.push(sa)
